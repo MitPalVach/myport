f62ad95a8e7f00bedeb1e36d06e017a76c3fb683
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-03-17 03:54:41 +0300

    left menu overflow: hidden abort menu by esc & overlay click

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,52 +1,29 @@
-const burger = document.querySelector('.burger'),
-    menu = document.querySelector('.menu'),
-    closeBtn = document.querySelector('.menu__close');
+window.addEventListener('DOMContentLoaded', () => {
 
-burger.addEventListener('click', () => {
-    menu.classList.add('active');
+    const burger = document.querySelector('.burger'),
+        menu = document.querySelector('.menu'),
+        closeBtn = document.querySelector('.menu__close'),
+        overlay = document.querySelector('.menu__overlay');
+
+    burger.addEventListener('click', () => {
+        menu.classList.add('active');
+        document.body.style.overflow = 'hidden';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        menu.classList.remove('active');
+        document.body.style.overflow = '';
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        document.body.style.overflow = '';
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && menu.classList.contains('active')) {
+            menu.classList.toggle('active');
+        }
+        document.body.style.overflow = '';
+    });
 });
-
-closeBtn.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
